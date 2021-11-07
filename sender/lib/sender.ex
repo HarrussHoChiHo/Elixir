3cdcfc2520844dd1ace325a7de3f5d782447a976
ziemaytausoul <ziemaytausoul@gmail.com>
2021-11-07 11:57:55 +0800

    Second commit Last pages: 33

--- sender/lib/sender.ex
+++ sender/lib/sender.ex
@@ -17,4 +17,18 @@ defmodule Sender do
     IO.puts("Email to #{email} sent")
     {:ok, "email_sent"}
   end
+
+  def notify_all(emails) do
+    emails
+
+    |> Enum.map( fn email ->
+      Task.async(fn ->
+        send_email(email)
+      end)
+    end)
+
+    |>Enum.map(&Task.await/1)
+
+  end
+
 end
