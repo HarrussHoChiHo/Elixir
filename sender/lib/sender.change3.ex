4bace77a7394634f49c752e88a5b4cde8caa0662
ziemaytausoul <ziemaytausoul@gmail.com>
2021-11-18 22:20:56 +0800

    11/18/2021 22:20

---
+++ sender/lib/sender.change3.ex
@@ -0,0 +1,14 @@
+defmodule Change3 do
+
+  def send_email(email) do
+    Process.sleep(3000)
+    IO.puts("Email to #{email} sent")
+    {:ok, "email_sent"}
+  end
+
+  def notify_all(emails) do
+    emails
+    |> Task.async_stream(&send_email/1, ordered: false)
+    |> Enum.to_list()
+  end
+end
