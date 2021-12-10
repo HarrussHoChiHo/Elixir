8893201e2e4674d01b9333e544f832d0e1c113bc
ziemaytausoul <ziemaytausoul@gmail.com>
2021-12-10 11:02:55 +0800

    DynamicSupervisor

--- jobber/lib/jobber.ex
+++ jobber/lib/jobber.ex
@@ -12,9 +12,25 @@ defmodule Jobber do
       :world
 
   """
-  alias Jobber.{JobRunner, Job}
+  alias Jobber.{JobRunner, JobSupervisor}
 
   def start_job(args) do
-    DynamicSupervisor.start_child(JobRunner, {Job, args})
+    DynamicSupervisor.start_child(JobRunner, {JobSupervisor, args})
+  end
+end
+
+defmodule Jobber.JobSupervisor do
+  use Supervisor, restart: :temporary
+
+  def start_link(args) do
+    Supervisor.start_link(__MODULE__, args)
+  end
+
+  def init(args) do
+    children = [{Jobber.Job, args}]
+
+    options = [strategy: :one_for_one, max_seconds: 30]
+
+    Supervisor.init(children, options)
   end
 end
