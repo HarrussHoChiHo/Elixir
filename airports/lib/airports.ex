fc9663e1257dd2dde29fc090cd5a6000958db0b4
ziemaytausoul <ziemaytausoul@gmail.com>
2022-01-08 21:28:51 +0800

    airports

--- airports/lib/airports.ex
+++ airports/lib/airports.ex
@@ -8,21 +8,33 @@ defmodule Airports do
     Application.app_dir(:airports, "/priv/airports.csv")
   end
 
-
   def open_airports() do
+    window = Flow.Window.trigger_every(Flow.Window.global(), 1000)
     airport_csv()
     |> File.stream!()
-    |> Flow.from_enumerable()
-    |> Flow.map(fn row ->
-      [row] = CSV.parse_string(row, skip_headers: false)
-      %{
-        id: Enum.at(row, 0),
-        type: Enum.at(row, 2),
-        name: Enum.at(row, 3),
-        country: Enum.at(row, 8)
-      }
+    |> Stream.map(fn event ->
+      Process.sleep(Enum.random([0,0,0,1]))
     end)
-    |> Flow.reject(&(&1.type == "closed"))
+    |> Flow.from_enumerable()
+    |> Flow.partition(window: window, key: {:key, :contry})
+    |> Flow.group_by(& &1.country)
+    |> Flow.on_trigger(fn acc, _partition_info, {_type, _id, trigger} ->
+      events =
+        acc
+        |> Enum.map(fn {country, data} ->
+          {country, Enum.count(data)}
+        end)
+        |> IO.inspect(label: inspect(self()))
+
+        case trigger do
+          :done ->
+            {events, acc}
+          {:every, 1000} ->
+            {[], acc}
+        end
+
+    end)
     |> Enum.to_list()
+
   end
 end
